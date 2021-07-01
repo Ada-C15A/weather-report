e3bd148b1db1234846874cb0077be85e346d5f28
peacegypsy <christinasherman206@gmail.com>
2021-07-01 08:41:58 -0700

    updated temp change and added color change

--- scripts/index.js
+++ scripts/index.js
@@ -1,12 +1,59 @@
-function incrementValue() {
-    let value = parseInt(document.getElementById('number').value);
-    value++;
-    document.getElementById('number').value = value;
+
+
+// function incrementValue() {
+//     let value = parseInt(document.getElementById('number').value);
+//     value++;
+//     document.getElementById('number').value = value;
+// }
+
+// function decrementValue() {
+//     let value = parseInt(document.getElementById('number').value);
+//     value--;
+//     document.getElementById('number').value = value;
+// }
+// function colorChange() {
+//     let num = parseInt(document.getElementById('#number').textContent.value);
+//     if (num.value === 56) {
+//         num.style.color = 'red';
+//     }
+// }
+
+const state = {
+    temperature: 55
+};
+
+const tempIncrease = (event) => {
+    console.log({ state });
+    state.temperature += 1; 
+    const tempDisplay = document.querySelector("#number");
+    tempDisplay.textContent = state.temperature;
+    if (state.temperature >= 56 && state.temperature <= 60) {
+        tempDisplay.style.color = "#1e81b0";
+    } else if (state.temperature >= 61 && state.temperature <= 70){
+        tempDisplay.style.color = "#e28743"
+    }
+};
+
+const tempDecrease = (event) => {
+    state.temperature -= 1;
+    const tempDisplay = document.querySelector("#number");
+    tempDisplay.textContent = state.temperature;
+    if (state.temperature >= 40 && state.temperature <= 55){
+        tempDisplay.style.color = "#abdbe3"
+    }
+};
+const registerEventHandlers = (event) => {
+    const upArrow = document.querySelector("#up");
+    upArrow.addEventListener("click", tempIncrease);
+    const downArrow = document.querySelector("#down");
+    downArrow.addEventListener("click", tempDecrease);
+};
+
+const changeColor = (event) => {
+    const tempDisplay = document.querySelector("#number").textContent;
+    if (state.temperature == 56) {
+        tempDisplay.textContent.fontcolor = "green";
+    }
 }
 
-function decrementValue() {
-    let value = parseInt(document.getElementById('number').value);
-    value--;
-    document.getElementById('number').value = value;
-}
-
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
