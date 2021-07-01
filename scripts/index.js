8e1706ccadce92e30bda0bda8fd7bddea62ca04e
peacegypsy <christinasherman206@gmail.com>
2021-07-01 14:26:15 -0700

    added sky change feature, works with dropdown menu

--- scripts/index.js
+++ scripts/index.js
@@ -26,34 +26,52 @@ const tempIncrease = (event) => {
     console.log({ state });
     state.temperature += 1; 
     const tempDisplay = document.querySelector("#number");
+    const photo = document.querySelector("#landscape");
     tempDisplay.textContent = state.temperature;
     if (state.temperature >= 56 && state.temperature <= 60) {
         tempDisplay.style.color = "#1e81b0";
     } else if (state.temperature >= 61 && state.temperature <= 70){
-        tempDisplay.style.color = "#e28743"
+        tempDisplay.style.color = "#e28743";
+        photo.src = ("assets/images/spring-sun-clouds.png");
+    } else if (state.temperature >= 71 && state.temperature <= 80){
+        tempDisplay.style.color = "#eab676";
+        photo.src="assets/images/very-warm.png";
+    }else if (state.temperature >= 81 && state.temperature <= 85){
+        tempDisplay.style.color = "#F8D210;"
+        photo.src="assets/images/beach.png";
+    } else if (state.temperature >= 86 && state.temperature <= 95) {
+        tempDisplay.style.color = "#FA26A0";
+        photo.src="assets/images/hot.png";
     }
 };
 
 const tempDecrease = (event) => {
     state.temperature -= 1;
     const tempDisplay = document.querySelector("#number");
+    const photo = document.querySelector("#landscape");
     tempDisplay.textContent = state.temperature;
     if (state.temperature >= 40 && state.temperature <= 55){
-        tempDisplay.style.color = "#abdbe3"
+        tempDisplay.style.color = "#abdbe3";
+        photo.src="assets/images/city-gentle-rain.png";
+    } else if (state.temperature >= 33 && state.temperature <=39){
+        tempDisplay.style.color = "#154c79";
+        photo.src="assets/images/very-snowy-city.png";
     }
 };
+
+function changeSky() {
+    const sky = document.querySelector("#sky-type");
+    var img = document.getElementById("mySelect");
+    var value = img.options[img.selectedIndex].value;
+    sky.src=value;
+}
 const registerEventHandlers = (event) => {
     const upArrow = document.querySelector("#up");
     upArrow.addEventListener("click", tempIncrease);
     const downArrow = document.querySelector("#down");
     downArrow.addEventListener("click", tempDecrease);
+    const skyChooser = document.querySelector("#mySelect");
+    skyChooser.addEventListener("change", changeSky);
 };
-
-const changeColor = (event) => {
-    const tempDisplay = document.querySelector("#number").textContent;
-    if (state.temperature == 56) {
-        tempDisplay.textContent.fontcolor = "green";
-    }
-}
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
