c127b6e69802deafee8e32a7f124b436b00b4b7b
trish <pmgriego@gmail.com>
2021-07-01 22:54:44 -0700

    make decrease temp count work

--- scripts/index.js
+++ scripts/index.js
@@ -3,12 +3,21 @@ const state = {
 }
 
 const increaseTemperature = () => {
-    let temperature = document.getElementById("temperature");
+    const temperature = document.getElementById("temperature");
     state.temperature += 1;
     temperature.innerHTML = state.temperature;
 
-    const temperatureCount = document.querySelector("temperature");
-    temperatureCount.textContent = state.temp;
+    const temperatureCount = document.querySelector("#temperature");
+    temperatureCount.textContent = state.temperature;
+}
+
+const decreaseTemperature = () => {
+    const temperature = document.getElementById("temperature");
+    state.temperature -= 1;
+    temperature.innerHTML = state.temperature;
+
+    const temperatureCount = document.querySelector("#temperature");
+    temperatureCount.textContent = state.temperature;
 }
 
 const registerEventHandlers = (event) => {
@@ -16,7 +25,7 @@ const registerEventHandlers = (event) => {
     increaseTemp.addEventListener("click", increaseTemperature);
 
     const decreaseTemp = document.querySelector("#decreaseTemperature");
-    decreaseTemp.addEventListener("click", decreaseTemperature);
+    decreaseTemp.addEventListener("click", decreaseTemperature)
 }
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
