e4481db51ea0fcf75433cd5a2c11df5194d23d6d
Karolina Benitez <kbeniitez@gmail.com>
2021-07-01 22:44:43 -0700

    Adds necessary html elements

---
+++ scripts/index.js
@@ -0,0 +1,33 @@
+weather_info = {
+    cool = {
+        fontColor: "#8cd1ec",
+        color: "#8cd1ec",
+        emojis = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃",
+        temp = [69,60]
+    },
+    warm = {
+        fontColor: "#e5b366",
+        color = "FFE1B4",
+        emojis = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷",
+        temp = [79,70]
+    },
+    hot = {
+        fontColor: "#ec0c0c",
+        color: "#ec0c0c",
+        emojis = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂",
+        temp = [200, 80]
+    },
+    freezing = {
+        fontColor: "#3f63c5",
+        color: "#3f63c5",
+        emojis = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲",
+        temp = [59, -100]
+    }
+}
+
+sky_info = {
+    sunny = "https://images.unsplash.com/photo-1473496169904-658ba7c44d8a?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1650&q=80",
+    cloudy = "https://images.unsplash.com/photo-1479688895406-3f032f15f0ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    rainy = "https://images.unsplash.com/photo-1610741083757-1ae88e1a17f7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80",
+    snowy = "https://images.unsplash.com/photo-1518873890627-d4b177c06e51?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80"
+}
