bd8d9388b3ac5762e4bd8ba987e5b7c5b3d96027
Cloudy Lopez <CloudyLopez@icloud.com>
2021-07-01 18:34:03 -0700

    trying to tie all the pieces together. i have added a grid to the css, made incremental additions to index.js and next step is to flesh out more of the html w btns

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,17 @@
+let tempStart = 71;
 
+const updateTemp = () => {
+  const tempCounter = document.getElementById("tempStart");
+  tempCounter.innerText = tempStart;
+}
 
 const increaseTemp = () => {
-
+  tempStart += 1;
+  updateTemp(tempStart)
 }
 
 const decreaseTemp = () => {
-
+  tempStart -= 1;
+  updateTemp(tempStart)
 }
 
