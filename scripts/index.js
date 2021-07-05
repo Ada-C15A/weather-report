cc733ba97ef94d6c0aaaac1b2d4d90ef610e2d1e
Naomi Quinones <naomiquinones@gmail.com>
2021-07-05 11:39:39 -0700

    Wave 2: buttons increase and decrease temperature

--- scripts/index.js
+++ scripts/index.js
@@ -1,25 +1,55 @@
+const state = {
+  city: "Berkeley",
+  temp: 62
+}
+const setTemp = event => {
+  if (event) {
+    newVal = parseInt(event.target.value);
+    state.temp += newVal;
+  }
+  let todaysTemp = document.getElementById("temp-today");
+  todaysTemp.innerHTML = state.temp;
+  setTodaysTemperatureColor();
+}
+
 const setTodaysTemperatureColor = () => {
-  tempTodayP = document.getElementById("temp-today");
-  todaysTemp = parseInt(tempTodayP.innerHTML);
-  console.log({ tempTodayP, todaysTemp });
-  if (todaysTemp >= 40 && todaysTemp < 50) {
+  const temp = state.temp;
+  const tempToday = document.getElementById("temp-today");
+  if (temp >= 40 && temp < 50) {
     todaysColorCode = "#00b8b8";
-  } else if (todaysTemp >= 50 && todaysTemp < 60) {
+  } else if (temp >= 50 && temp < 60) {
     todaysColorCode = "green";
-  } else if (todaysTemp >= 60 && todaysTemp < 70) {
+  } else if (temp >= 60 && temp < 70) {
     todaysColorCode = "yellow";
-  } else if (todaysTemp >= 70 && todaysTemp < 80) {
+  } else if (temp >= 70 && temp < 80) {
     todaysColorCode = "orange";
-  } else if (todaysTemp >= 80 && todaysTemp < 90) {
+  } else if (temp >= 80 && temp < 90) {
     todaysColorCode = "red";
-  } else if (todaysTemp >= 90 && todaysTemp < 100) {
+  } else if (temp >= 90 && temp < 100) {
     todaysColorCode = "darkred";
-  } else if (todaysTemp >= 100) {
+  } else if (temp >= 100) {
     todaysColorCode = "#831";
   }
-  tempTodayP.style.color = todaysColorCode;
+  tempToday.style.color = todaysColorCode;
 };
 
+const setCity = () => {
+  cityP = document.getElementById("city-display");
+  cityP.innerHTML = state.city;
+}
+// check if button got pressed
+// then either increase or decrease the 
+
+const setHandlers = () => {
+  document.getElementById("raise-temp")
+          .addEventListener("click",setTemp);
+  document.getElementById("lower-temp")
+          .addEventListener("click",setTemp);
+}
+
 window.onload = () => {
+  setHandlers();
   setTodaysTemperatureColor();
+  setTemp();
+  setCity();
 }
