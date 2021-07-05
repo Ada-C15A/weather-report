6f3e728ee897095010b9cfcbc8b538532e979747
Andrea Valliere <amvalliere@gmail.com>
2021-07-04 18:07:54 -0700

    adds ability to change sky emojis via dropdown

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,19 @@
 input.oninput = () => {
     currentCity.innerHTML = input.value;
 };
+
+const displaySky = () => {
+    const skies = {
+        'cloudy': '☁️☁️☁️☁️',
+        'clear': '☀️☀️😎☀️',
+        'snowy': '❄️🌨❄️❄️',
+        'stormy': '⚡️⛈⚡️🌪',
+        'fire': '🔥🌫☄️🔥'
+    }
+    const selectedSky = document.getElementById("skySelect").value;
+
+    let sky = skies[selectedSky]
+
+    skyGraphics.innerHTML = sky;
+}
+
