1b3cc818e09381b8af360b4ba9beea9b1662fde9
Naomi Quinones <naomiquinones@gmail.com>
2021-07-05 17:15:52 -0700

    Capture click on child element, rename fxn

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,28 @@
 const state = {
   city: "Berkeley",
-  temp: 62
-}
-const setTemp = event => {
+  temp: 52,
+};
+const setTemp = (event) => {
   if (event) {
-    newVal = parseInt(event.target.value);
+    newVal = event.target.value ?
+      parseInt(event.target.value) :
+      parseInt(event.target.dataset.temp);
     state.temp += newVal;
   }
   let todaysTemp = document.getElementById("temp-today");
   todaysTemp.innerHTML = state.temp;
-  setTodaysTemperatureColor();
-}
+  updateUI();
+};
 
-const setTodaysTemperatureColor = () => {
+const updateUI = () => {
   const temp = state.temp;
   const tempToday = document.getElementById("temp-today");
-  if (temp >= 40 && temp < 50) {
+  if (temp >= 30 && temp < 40) {
+    todaysColorCode = "#2de";
+  } else if (temp >= 40 && temp < 50) {
     todaysColorCode = "#00b8b8";
   } else if (temp >= 50 && temp < 60) {
-    todaysColorCode = "green";
+    todaysColorCode = "#1b5";
   } else if (temp >= 60 && temp < 70) {
     todaysColorCode = "yellow";
   } else if (temp >= 70 && temp < 80) {
@@ -30,26 +34,24 @@ const setTodaysTemperatureColor = () => {
   } else if (temp >= 100) {
     todaysColorCode = "#831";
   }
+  console.log({ temp, todaysColorCode });
   tempToday.style.color = todaysColorCode;
 };
 
 const setCity = () => {
   cityP = document.getElementById("city-display");
   cityP.innerHTML = state.city;
-}
+};
 // check if button got pressed
-// then either increase or decrease the 
+// then either increase or decrease the
 
 const setHandlers = () => {
-  document.getElementById("raise-temp")
-          .addEventListener("click",setTemp);
-  document.getElementById("lower-temp")
-          .addEventListener("click",setTemp);
-}
+  document.getElementById("raise-temp").addEventListener("click", setTemp);
+  document.getElementById("lower-temp").addEventListener("click", setTemp);
+};
 
-window.onload = () => {
+window.addEventListener("load", (event) => {
   setHandlers();
-  setTodaysTemperatureColor();
   setTemp();
   setCity();
-}
+});
