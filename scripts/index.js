993cbc9088f87108efcc886831895e186ed3884f
Cloudy Lopez <CloudyLopez@icloud.com>
2021-07-06 21:12:45 -0700

    added some more functionality to it.

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,31 @@
 let tempStart = 71;
 
-const updateTemp = () => {
+const updateCityName = () => {
+  const updatedCityName = document.getElementById("cityName").value;
+}
+
+const resetCityName = () => {
+  const cityName = document.getElementById("cityName");
+  cityName.value = "Los Angeles";
+  updateCityName();
+}
+
+const updateGarden = currentTemp => {
+  const emojiWeather = document.getElementById("emoji");
+  let emoji = "🌲🌲⛄️🌲⛄️🍂🌲🍁🌲🌲⛄️🍂🌲";
+  if (currentTemp >= 80) {
+    emoji = "🌵__🐍_🦂_🌵🌵__🐍_🏜_🦂";
+  } else if (currentTemp >= 70) {
+    emoji = "🌸🌿🌼__🌷🌻🌿_☘️🌱_🌻🌷";
+  } else if (currentTemp >= 70) {
+    emoji = "🌾🌾_🍃_🪨__🛤_🌾🌾🌾_🍃";
+  }
+  emojiWeather.textContent = emoji;
+}
+
+const updateTemp = tempStart => {
   const tempCounter = document.getElementById("tempStart");
-  tempCounter.innerText = tempStart;
+  tempCounter.textContent = tempStart;
 }
 
 const increaseTemp = () => {
@@ -15,3 +38,17 @@ const decreaseTemp = () => {
   updateTemp(tempStart)
 }
 
+const registerEventHandlers = () => {
+  updateTemp(tempStart);
+
+  const increaseTempBtn = document.getElementById("increaseTempBtn");
+  increaseTempBtn.addEventListener("click", increaseTemp);
+
+  const decreaseTempBtn = document.getElementById("decreaseTempBtn");
+  decreaseTempBtn.addEventListener("click", decreaseTemp);
+
+  const cityNameResetBtn = document.getElementById("cityName");
+  cityNameResetBtn.addEventListener("click", resetCityName);
+};
+
+document.addEventListener("DOMContentLoaded", registerEventHandlers);
