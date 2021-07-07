dbc0ecee0c47cb4b044d0f17ece25c0bace0b2d2
Cloudy Lopez <CloudyLopez@icloud.com>
2021-07-06 22:16:57 -0700

    added more to .js file for functionality

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,59 @@
 let tempStart = 71;
 
+const headerCityName = () => {
+  const currentCityHeader = document.getElementById("currentCityHeader");
+  currentCityHeader.textContent = updateCityName;
+}
+
 const updateCityName = () => {
   const updatedCityName = document.getElementById("cityName").value;
+  headerCityName();
 }
 
 const resetCityName = () => {
   const cityName = document.getElementById("cityName");
   cityName.value = "Los Angeles";
   updateCityName();
+  headerCityName();
+}
+
+const updateSky = () => {
+  const skyValue = document.getElementById("skyReflection").value;
+  const skyCurrent = document.getElementById("sky")
+
+  let sky = "";
+  let skyColor = "";
+  if (skyValue === "Cloudy") {
+    sky = "☁️☁️ ☁️ ☁️☁️ ☁️ 🌤 ☁️ ☁️☁️";
+    skyColor = "cloudy"
+  } else if (skyValue === "Sunny") {
+    sky = "☁️ ☁️ ☁️ ☀️ ☁️ ☁️";
+    skyColor = "sunny"
+  } else if (skyValue ==="Rainy") {
+    sky = "🌧🌈⛈🌧🌧💧⛈🌧🌦🌧💧🌧🌧";
+    skyColor = "rainy"
+  } else if (skyValue === "Snowy") {
+    sky = "🌨❄️🌨🌨❄️❄️🌨❄️🌨❄️❄️🌨🌨"
+    skyColor = "snowy"
+  }
+  skyCurrent.textContent = sky;
+  const gardenContent = document.getElementById("gardenContent");
+  gardenContent.classList = `garden_content ${skyColor}`
+};
+
+const updateTempColoring = currentTemp => {
+  const tempStartValue = document.getElementById("tempStart");
+  let color = "teal";
+  if (currentTemp >= 80) {
+    color = "red";
+  } else if (currentTemp >= 70) {
+    color = "orange";
+  } else if (currentTemp >= 60) {
+    color = "yellow";
+  } else if (currentTemp >= 50) {
+    color = "green";
+  }
+  tempStartValue.classList = color;
 }
 
 const updateGarden = currentTemp => {
@@ -26,6 +72,8 @@ const updateGarden = currentTemp => {
 const updateTemp = tempStart => {
   const tempCounter = document.getElementById("tempStart");
   tempCounter.textContent = tempStart;
+  updateTempColoring(tempStart);
+  updateGarden(tempStart);
 }
 
 const increaseTemp = () => {
@@ -47,8 +95,16 @@ const registerEventHandlers = () => {
   const decreaseTempBtn = document.getElementById("decreaseTempBtn");
   decreaseTempBtn.addEventListener("click", decreaseTemp);
 
+  updateCityName();
+  const cityName = document.getElementById("cityName");
+  cityName.addEventListener("input", updateCityName)
+
   const cityNameResetBtn = document.getElementById("cityName");
   cityNameResetBtn.addEventListener("click", resetCityName);
+
+  updateSky();
+  const skyReflection = document.getElementById("skyReflection");
+  skyReflection.addEventListener("change", updateSky)
 };
 
 document.addEventListener("DOMContentLoaded", registerEventHandlers);
